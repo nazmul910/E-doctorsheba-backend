af29676f8f920623ce62235d308f8cb620b6e28d
MD Nazmul Hassan <160168720+nazmul910@users.noreply.github.com>
2025-06-24 02:57:55 +0600

    add env

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async()=>{
   mongoose.connection.on('connected',()=> console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MOnGODB_URI}/e-doctorsheba`); //eikhane /e-doctorsheba dara database ei name toiri hobe tai dewya hoyeche
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-doctorsheba`); //eikhane /e-doctorsheba dara database ei name toiri hobe tai dewya hoyeche
 }
 
-export default connectDB;
+export default connectDB; 
